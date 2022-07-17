ac9f4fccf75307dff6966e7eaeeb22c320ae99d1
Fawzi <waseela.jo@icloud.com>
2022-07-17 12:21:16 +0300

    Display a label with white text centered vertically and horizontally on a black background. Display a button that toggles the color when tapped.

--- NoviceChallenge1/ViewController.swift
+++ NoviceChallenge1/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var statusBarStyle = UIStatusBarStyle.lightContent
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        statusBarStyle
+    }
+    
+    @IBAction func toggleColor(_ sender: Any) {
+        if view.backgroundColor == .white {
+            statusBarStyle = .lightContent
+            view.backgroundColor = .black
+            label.textColor = .white
+        } else {
+            statusBarStyle = .darkContent
+            view.backgroundColor = .white
+            label.textColor = .black
+        }
+        setNeedsStatusBarAppearanceUpdate()
     }
 
-
 }
-
